c635e8231835e86c01ce9c79a02ba419532d213a
Oleksandr Avoyants <shhavel@gmail.com>
2017-03-22 20:28:09 +0200

    Fix locale in navigation links

--- templates/layouts/navigation_links.php
+++ templates/layouts/navigation_links.php
@@ -6,47 +6,47 @@
 <?php
 if (isset($conn) && !$conn->connect_errno) {
   while ($row = $g_courses->fetch_assoc()) {
-    echo '<li><a href="/uk-'.$row['course'].'">'.$row[$title].'</a></li>';
+    echo '<li><a href="/'.$locale.'-'.$row['course'].'">'.$row[$title].'</a></li>';
   }
 } else {
   foreach ($g_courses as $row) {
-    echo '<li><a href="/uk-'.$row['course'].'">'.$row[$title].'</a></li>';
+    echo '<li><a href="/'.$locale.'-'.$row['course'].'">'.$row[$title].'</a></li>';
   }
 }
 ?>
   </ul>
 </li>
 <li class="dropdown<?php echo isset($category) && $category == 'wh' ? ' active' : '' ?>">
-  <a href="/uk-wh" class="dropdown-toggle" data-toggle="dropdown">
+  <a href="/<?php echo $locale; ?>-wh" class="dropdown-toggle" data-toggle="dropdown">
     <?php echo WORLD_HISTORY; ?> <b class="caret"></b>
   </a>
   <ul class="dropdown-menu">
 <?php
 if (isset($conn) && !$conn->connect_errno) {
   while ($row = $wh_courses->fetch_assoc()) {
-    echo '<li><a href="/uk-'.$row['course'].'">'.$row[$title].'</a></li>';
+    echo '<li><a href="/'.$locale.'-'.$row['course'].'">'.$row[$title].'</a></li>';
   }
 } else {
   foreach ($wh_courses as $row) {
-    echo '<li><a href="/uk-'.$row['course'].'">'.$row[$title].'</a></li>';
+    echo '<li><a href="/'.$locale.'-'.$row['course'].'">'.$row[$title].'</a></li>';
   }
 }
 ?>
   </ul>
 </li>
 <li class="dropdown<?php echo isset($category) && $category == 'uh' ? ' active' : '' ?>">
-  <a href="/uk-uh" class="dropdown-toggle" data-toggle="dropdown">
+  <a href="/<?php echo $locale; ?>-uh" class="dropdown-toggle" data-toggle="dropdown">
     <?php echo UKRAINE_HISTORY; ?> <b class="caret"></b>
   </a>
   <ul class="dropdown-menu">
 <?php
 if (isset($conn) && !$conn->connect_errno) {
   while ($row = $uh_courses->fetch_assoc()) {
-    echo '<li><a href="/uk-'.$row['course'].'">'.$row[$title].'</a></li>';
+    echo '<li><a href="/'.$locale.'-'.$row['course'].'">'.$row[$title].'</a></li>';
   }
 } else {
   foreach ($uh_courses as $row) {
-    echo '<li><a href="/uk-'.$row['course'].'">'.$row[$title].'</a></li>';
+    echo '<li><a href="/'.$locale.'-'.$row['course'].'">'.$row[$title].'</a></li>';
   }
 }
 ?>
