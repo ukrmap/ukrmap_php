ef450e548c765e042c8260ec3daba8323556fc77
Oleksandr Avoyants <shhavel@gmail.com>
2017-04-23 11:35:49 +0300

    Fix typo in CSS class name

--- templates/articles/navbar_lover.php
+++ templates/articles/navbar_lover.php
@@ -1,6 +1,6 @@
 <div class="navbar-default navbar-lower">
-  <div class="container container-navbar-lover">
-    <ul class="nav navbar-nav-lover">
+  <div class="container container-navbar-lower">
+    <ul class="nav navbar-nav-lower">
       <li><a href="/articles.html"><?php echo ARTICLES; ?></a></li>
     </ul>
   </div>
--- templates/layouts/navbar_lover.php
+++ templates/layouts/navbar_lover.php
@@ -1,6 +1,6 @@
 <div class="navbar-default navbar-lower">
-  <div class="container container-navbar-lover">
-    <ul class="nav navbar-nav-lover">
+  <div class="container container-navbar-lower">
+    <ul class="nav navbar-nav-lower">
       <li><a href="<?php echo '/'.$loc.'-'.$course; ?>"><?php echo $parent[$title]; ?></a></li>
     </ul>
   </div>
