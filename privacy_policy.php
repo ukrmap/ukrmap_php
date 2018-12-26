429640ba61339e377c50cbfdc25770dca997aad8
Alex Avoyants <shhavel@gmail.com>
2018-12-26 23:58:54 +0200

    Merge branch 'master' into static

---
+++ privacy_policy.php
@@ -0,0 +1,8 @@
+<?php
+require('source/setup_without_mysql.php');
+require('templates/layouts/header.php');
+require('templates/layouts/navigation.php');
+require('templates/layouts/container.php');
+require('templates/layouts/privacy_policy_'.$locale.'.php');
+require('templates/layouts/footer_light.php');
+?>
