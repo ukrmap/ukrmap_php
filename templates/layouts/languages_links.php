1441a5370349057a8683590a44ed579e3a8da5ff
Alex Avoyants <shhavel@gmail.com>
2018-12-26 23:58:32 +0200

    Privacy Policy

--- templates/layouts/languages_links.php
+++ templates/layouts/languages_links.php
@@ -55,5 +55,21 @@
     <?php } ?>
     </ul>
   </li>
+<?php } elseif ($_SERVER['SCRIPT_NAME'] == '/privacy_policy.php') { ?>
+  <li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+      <img src="/css/images/blank.png" class="flag flag-<?php echo $locale ?>" alt="Select language"/> <?php echo $languages[$locale] ?> <b class="caret"></b>
+    </a>
+    <ul class="dropdown-menu">
+    <?php foreach ($languages as $loc => $language) { if ($loc == $locale) { continue; } ?>
+      <li>
+        <a href="/<?php echo $loc.'/privacy_policy.html'; ?>">
+          <img src="/css/images/blank.png" class="flag flag-<?php echo $loc ?>" alt="Select language"/>
+          <?php echo $language; ?>
+        </a>
+      </li>
+    <?php } ?>
+    </ul>
+  </li>
 <?php } ?>
 </ul>
